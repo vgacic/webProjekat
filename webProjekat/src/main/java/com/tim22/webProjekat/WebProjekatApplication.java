410bd86a72fb5660079f718e06ae4a6c80dd6d2e
vgacic <vgacic02@gmail.com>
2023-04-12 14:11:38 +0200

    Dodavanje inicijalnog projekta

---
+++ webProjekat/src/main/java/com/tim22/webProjekat/WebProjekatApplication.java
@@ -0,0 +1,13 @@
+package com.tim22.webProjekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProjekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProjekatApplication.class, args);
+	}
+
+}
---
+++ webProjekat/src/test/java/com/tim22/webProjekat/WebProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim22.webProjekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
